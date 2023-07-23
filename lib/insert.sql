a90845294564413683803c99256eb6346e1fbdef
Justin Bigishiro <justin.rvd@gmail.com>
2023-07-23 11:30:07 -0500

    adds my file

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id,name, age, sex, color, temperament, alive) VALUES 
+(1, "Mr. Chocolate", 5, "M", "Black", "Furous", 1),
+(2, "Rowdy", 7, "M", "White", "Calm", 0), 
+(3, "Tabitha", 5, "F", "Black", "Happy", 1),
+(4, "Sergeant Brown", 3, "M", "Black", "Good", 0),
+(5, "Melissa", 9, "F", "Grey", "Dangerous", 1),
+(6, "Grinch", 3, "M", "Black", "Furous", 0),
+(7, "Wendy", 6, "F", "White", "Good", 1),
+(8, NULL, 3, "M", "Grey", "Happy", 1); 
